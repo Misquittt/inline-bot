f1da065e96914a357681506ff6de6e83f78fa691
Misquittt <sheellshock7@gmail.com>
2025-07-15 15:54:31 +0200

    Update inline_bot.py

--- inline_bot.py
+++ inline_bot.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineQueryResultArticle, InputTextMessageContent
 
 API_TOKEN = '7621915611:AAEb0BR4L7dKtQO5Jc1Gd3h9XHD-6rCihn4'
-ALLOWED_USER_IDS = [1234567890]
+ALLOWED_USER_IDS = [7362675412]
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
